a8d31e3eb9b66b7c761e7a9e4dcf493bdb38b50f
thevoid <thevoid@thevoid-Lenovo-V14-G3-IAP>
2025-04-24 18:23:47 -0600

    Dependecias

---
+++ src/main/java/com/mycompany/systemforreform/SystemForReform.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.systemforreform;
+
+/**
+ *
+ * @author thevoid
+ */
+public class SystemForReform {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
